b2dbf9d993979ac81621aeb320e7a8b4529665a7
voidstarstar <voidstarstar@users.noreply.github.com>
2016-06-22 18:06:17 -0400

    Move the ThreadPoolExecutor
    The FiberExecutorScheduler subclass is now responsible for the
    ThreadPoolExecutor, PriorityBlockingQueue, and Comparator.

--- src/main/java/example/example/quasar/QFiberExecutorScheduler.java
+++ src/main/java/example/example/quasar/QFiberExecutorScheduler.java
@@ -1,16 +1,30 @@
 package example.example.quasar;
 
-import java.util.concurrent.Executor;
+import java.util.Comparator;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import co.paralleluniverse.fibers.FiberExecutorScheduler;
+import co.paralleluniverse.fibers.FiberSchedulerTask;
 import co.paralleluniverse.strands.SuspendableCallable;
 
 public class QFiberExecutorScheduler extends FiberExecutorScheduler {
 
+	private static final int parallelism = Runtime.getRuntime().availableProcessors();
+	private static final Comparator<? super Runnable> comparator = (a, b) -> {
+		FiberSchedulerTask fiberSchedulerTaskA = (FiberSchedulerTask) a;
+		FiberSchedulerTask fiberSchedulerTaskB = (FiberSchedulerTask) b;
+		QFiber fiberA = (QFiber) fiberSchedulerTaskA.getFiber();
+		QFiber fiberB = (QFiber) fiberSchedulerTaskB.getFiber();
+		return fiberA.compareTo(fiberB);
+	};
+
 	private Comparable comparable;
 
-	public QFiberExecutorScheduler(String name, Executor executor) {
-		super(name, executor);
+	public QFiberExecutorScheduler(String name) {
+		super(name, new ThreadPoolExecutor(parallelism, parallelism, Long.MAX_VALUE, TimeUnit.NANOSECONDS,
+				new PriorityBlockingQueue<Runnable>(11, comparator)));
 	}
 
 	@Override
